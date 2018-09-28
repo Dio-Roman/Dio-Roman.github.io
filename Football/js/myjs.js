d0c52b3e3c73b49ca119e236889ae88aa7c18999
Dio-Roman <dio-roman@mail.ru>
2018-09-28 20:21:02 +0300

    date update

--- Football/js/myjs.js
+++ Football/js/myjs.js
@@ -3,10 +3,24 @@
 //  Выделить дни матчей
 let a = document.getElementsByClassName("datepicker--cell-day");
 
+
 function week() {
-    a.item(4).innerHTML = `5*`;
-    a.item(15).innerHTML = `16*`;
-    a.item(24).innerHTML = `25*`;
+    let date = new Date();
+    date.setDate(1);
+    let dayPosition = date.getDay();
+    let addCell;
+
+    if (dayPosition == 0) {
+        addCell = 6;
+    } else {
+        addCell = dayPosition-1;
+    }
+    
+    a.item(addCell+4).innerHTML = `5*`;
+    a.item(addCell+15).innerHTML = `16*`;
+    a.item(addCell+24).innerHTML = `25*`;
+
+    return date.toLocaleString('ru', {month: 'long'});
 }
 setTimeout(week, 100);
 
@@ -15,46 +29,44 @@ let day = document.querySelector("#date");
 
 day.onclick = function (event) {
     let d = event.target.getAttribute('data-date');
-    // var m = event.target.getAttribute('data-month') ; В этом примере не обязательно.
-    // var y = event.target.getAttribute('data-year') ;  В этом примере не обязательно.
 
     let game = document.createElement('div');
     game.className = "game z-depth-1";
     let length = document.getElementById('date').children.length;
-    let divCollection = document.getElementById('date').children;
+    console.log(document.getElementById('date').children);
 
-    const Jan5 = "<strong>5 января 2018</strong> <p>Россия - Египет (Стадион Санкт-Петербург - СПб).</p>";
-    const Jan16 = "<strong>16 января 2018</strong> <p>Испания - Италия (Стадион Лужники - Москва).</p>";
-    const Jan25 = "<strong>25 января 2018</strong> <p>Чехия - Германия (Стадион Фишт - Сочи).</p>";
+    const date5 = `<strong>5 ${week()} 2018</strong> <p>Россия - Египет (Стадион Санкт-Петербург - СПб).</p>`;
+    const date16 = `<strong>16 ${week()} 2018</strong> <p>Испания - Италия (Стадион Лужники - Москва).</p>`;
+    const date25 = `<strong>25 ${week()} 2018</strong> <p>Чехия - Германия (Стадион Фишт - Сочи).</p>`;
 
     switch (d) {
         case '5':
 
             if (length < 2) {
-                game.innerHTML = Jan5;
+                game.innerHTML = date5;
                 date.appendChild(game)
             } else {
-                document.querySelector(".game").innerHTML = Jan5
+                document.querySelector(".game").innerHTML = date5
             };
             break;
 
         case '16':
 
             if (length < 2) {
-                game.innerHTML = Jan16;
+                game.innerHTML = date16;
                 date.appendChild(game)
             } else {
-                document.querySelector(".game").innerHTML = Jan16
+                document.querySelector(".game").innerHTML = date16
             };
             break;
 
         case '25':
 
             if (length < 2) {
-                game.innerHTML = Jan25;
+                game.innerHTML = date25;
                 date.appendChild(game)
             } else {
-                document.querySelector(".game").innerHTML = Jan25
+                document.querySelector(".game").innerHTML = date25
             };
             break;
     }
